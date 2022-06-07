c42ef7e51829cadf0742af85629aedb654e4a458
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-07 00:11:42 -0400

    creando servidor basico

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hola mundo');
+const http = require('http');
+
+http.createServer((request,response) => {
+    response.write('Hola Mundo');
+    response.end();
+})
+.listen(8080);
+
+console.log('Escuchando el puerto', 8080);
+
