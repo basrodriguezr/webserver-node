daab72258737e53f939dc6233e180d137004afa2
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-07 00:35:02 -0400

    introduccion a express

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((request,response) => {
-    response.write('Hola Mundo');
-    response.end();
-})
-.listen(8080);
+// http.createServer((request,response) => {
+//     // console.log(request);
 
-console.log('Escuchando el puerto', 8080);
+//     // response.writeHead(200, {'Content-Type: application/json'});
 
+//     // const persona = {
+//     //     id: 1,
+//     //     nomnbre: 'Luffy'
+//     // };
+
+//     // response.write(JSON.stringify(persona));
+//     response.write('Hola Mundo');
+//     response.end();
+// })
+// .listen(8080);
+
+// console.log('Escuchando el puerto', 8080);
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/',  (req, res) => {
+  res.send('Dashboard vacío');
+
+});
+
+app.get('/hola-mundo',  (req, res) => {
+    res.send('Hello World');
+  
+});
+
+app.get('*',  (req, res) => {
+    //res.writeHead(404);
+    res.send('404 | Page not found');
+  
+});
+
+app.listen(port, ()=> {
+    console.log(`Escuchando el puerto ${port}`)
+});
